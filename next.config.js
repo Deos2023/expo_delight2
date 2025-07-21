b38e2929e5826e17c7ad552b83222e9c29c0af8c
Digital Esposure Online Services <131973180+Deos2023@users.noreply.github.com>
2025-07-21 12:03:07 +0530

    Update

--- next.config.js
+++ next.config.js
@@ -3,8 +3,5 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-     images: {
-    domains: ['images.unsplash.com', 'flagcdn.com','cdn-icons-png.flaticon.com'], // add any other domains you use
-  },
 }
 module.exports = nextConfig
